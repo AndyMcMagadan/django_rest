26111847d6cfa0d51615560fca4e4d25885d8397
AndyMcMagadan <mcmagadan@mail.ru>
2022-05-27 17:38:09 +0300

    API worked

--- library/authors/views.py
+++ library/authors/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import AuthorModelSerializer
+from .models import Author
 
-# Create your views here.
+
+class AuthorModelViewSet(ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorModelSerializer
